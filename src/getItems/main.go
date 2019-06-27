415171885db05e8f396eefdea358fe7e544a47c2
Anna Spysz <anna.spysz@stackery.io>
2019-06-27 14:57:25 -0700

    Initial function code

--- src/getItems/main.go
+++ src/getItems/main.go
@@ -1,17 +1,63 @@
 package main
 
 import (
-    "fmt"
-    "context"
-    "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+
+	"fmt"
+	"os"
 )
 
-func Handler(ctx context.Context, event interface{}) (string, error) {
-  fmt.Println(event)
+// Create struct to hold info about new item
+type Item struct {
+	Id      string
+	Content string
+}
 
-  return "OK", nil
+func Handler(ctx context.Context, event interface{}) (string, error) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	tableName := os.Getenv("TABLE_NAME") // get the table name from the automatically populated environment variables
+
+	// Build the query input parameters
+	params := &dynamodb.ScanInput{
+		TableName: aws.String(tableName),
+	}
+
+	// Use dynamodb to get items from the ItemTable
+	result, err := svc.Scan(params)
+	if err != nil {
+		fmt.Println("Error getting items from table " + tableName)
+		fmt.Println((err.Error()))
+		os.Exit(1)
+	}
+
+	for _, i := range result.Items {
+		item := Item{}
+
+		err = dynamodbattribute.UnmarshalMap(i, &item)
+
+		if err != nil {
+			fmt.Println("Got error unmarshalling:")
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("Item " + item.Id + ": " + item.Contents)
+		fmt.Println()
+	}
+
+	return result.Count + " items found"
 }
 
 func main() {
-  lambda.Start(Handler)
+	lambda.Start(Handler)
 }
--- src/postItem/main.go
+++ src/postItem/main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "context"
-    "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
+	"fmt"
+	"os"
+	"strconv"
 )
 
-func Handler(ctx context.Context, event interface{}) (string, error) {
-  fmt.Println(event)
+// Create struct to hold info about new item
+type Item struct {
+	Id      string
+	Content string
+}
 
-  return "OK", nil
+func Handler(ctx context.Context, event interface{}) (string, error) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+  svc := dynamodb.New(sess)
+  
+  item := Item{
+    Id:   "1", // modify with each invoke so the id does not repeat
+    Content:  "This is my content" // modify content here
+  }
+
+  input := &dynamodb.PutItemInput{
+    Item:      item,
+    TableName: aws.String(os.Getenv("TABLE_NAME")) // get the table name from the automatically populated environment variables
+  }
+
+  _, err = svc.PutItem(input)
+  if err != nil {
+      fmt.Println("Error adding " + itemId + " to table " + tableName )
+      fmt.Println(err.Error())
+      os.Exit(1)
+  }
+
+  fmt.Println("Adding item '" + itemId + " to table " + tableName)
 }
 
 func main() {
-  lambda.Start(Handler)
+	lambda.Start(Handler)
 }
