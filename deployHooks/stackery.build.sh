cd7add72d5a26d96749aa4c5b0436f1ecd91f798
Anna Spysz <anna.spysz@stackery.io>
2019-11-22 10:02:51 -0800

    Update deployHook script

--- deployHooks/stackery.build.sh
+++ deployHooks/stackery.build.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
-
 set -e
 mkdir -p .aws-sam/build/src/
 
-cp .stackery/template.yaml .aws-sam/build/template.yaml
-
-( cd src/getItems && GOBIN=$PWD GOPATH=$PWD make )
-
-rm -rf .aws-sam/build/src/getItems
-cp -r src/getItems .aws-sam/build/src/getItems
-
-( cd src/newItem && GOBIN=$PWD GOPATH=$PWD make )
-
-rm -rf .aws-sam/build/src/newItem
-cp -r src/newItem .aws-sam/build/src/newItem
+for function in src/*; do
+  echo compiling $function
+  ( cd $function && make )
+  rm -rf .aws-sam/build/$function
+  cp -r $function .aws-sam/build/$function
+done
